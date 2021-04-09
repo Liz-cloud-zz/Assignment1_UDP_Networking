0ad6128364ae2899366c8744f1cdef02422410ba
Liz-cloud <61148237+Liz-cloud@users.noreply.github.com>
2021-04-09 09:43:33 +0200

    Question1_update

---
+++ src/com/company/Receiver.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Receiver implements Runnable {
+    private int port;
+    private volatile boolean running;
+    private String name;
+
+    public Receiver(String myName, int portNum/*, boolean shouldRun*/) {
+        name = myName;
+        port = portNum;
+//        running = shouldRun;
+    }
+
+    public void setRunning(boolean run) {
+        running = run;
+    }
+
+    @Override
+    public void run() {
+        setRunning(true);
+        try {
+            while (running) {
+                DatagramSocket sktReceive = new DatagramSocket(port);
+                byte[] buf = new byte[1024]; //create empty byte buffer
+                DatagramPacket pktReceive = new DatagramPacket(buf, buf.length); //create temporary empty packet
+                sktReceive.receive(pktReceive); //receive packet from socket and store in packet
+                String msg = new String(pktReceive.getData(), 0, pktReceive.getLength()); //convert packet to message string
+                System.out.println(name + " says: " + msg); //output message received
+                sktReceive.close(); //close socket
+
+                if (msg.equals("end"))
+                    setRunning(false); //end loop (and thread) if server says end
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket error in Client Receiver: ");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("IO error in Client Receiver: ");
+            e.printStackTrace();
+        }
+    }
+}
